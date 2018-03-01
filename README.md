558cda5017dd701e8dc407acb2262ca53ce7857e
znlccy <756311868@qq.com>
2018-03-01 14:31:33 +0800

    添加说明文档

---
+++ README.md
@@ -0,0 +1,18 @@
+# 分布式开发系统
+
+## 整合MongoDB
+    Mongodb是一款很流行文档型数据库
+
+## 整合Redis
+    Redis是一款很流行的缓存型数据库
+
+## 整合MySQL
+    MySQL是关系型数据库
+
+## 整合MyBatis
+    MyBatis作为对象-关系型数据库持久层框架
+
+## 整合WebService
+
+## 整合WebSocket
+    WebSocket作为消息通信机制的Web系统
--- src/main/java/com/znlccy/distribute/DistributeApplication.java
+++ src/main/java/com/znlccy/distribute/DistributeApplication.java
@@ -2,8 +2,10 @@ package com.znlccy.distribute;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DistributeApplication {
 
 	public static void main(String[] args) {
